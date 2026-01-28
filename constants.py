1dfd142192befef68f4db51fff3cf1cc34a9fd5f
billel <billel.boulahia456@gmail.com>
2026-01-28 22:16:16 +0100

    adding constants

---
+++ constants.py
@@ -0,0 +1,18 @@
+URL = "http://www.concours-fonction-publique.gov.dz/ar/Liste.asp"
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0",
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+date_name = "تـاريخ الإدراج في الموقــع"
+
+locatisation=16
+
+day_limit=15
+
+license=4
+master=2
+
+
+master_json = "Master_offers.json"
+license_json = "License_offers.json"
