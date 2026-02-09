fa3185c52093a45c92234618bbef32eb05c6a0e3
billel <billel.boulahia456@gmail.com>
2026-02-09 01:39:03 +0100

    removed duplication in main

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ from offer import fetch_offers
 from constants import license, master, master_json, license_json
 from utils import add
 
+configs = [
+    (license, license_json, "license"),
+    (master, master_json, "master"),
+]
 
-for offer in fetch_offers(niv_etude=license):
-    if add(offer, license_json):
-        print(f"icense offer added: {offer.get('مكان إيداع أو إرسال ملفات الترشح')}")
-
-
-for offer in fetch_offers(niv_etude=master):
-    if add(offer, master_json):
-        print(f"master offer added: {offer.get('مكان إيداع أو إرسال ملفات الترشح')}")
-
-
+for niv_etude, json_file, label in configs:
+    for offer in fetch_offers(niv_etude=niv_etude):
+        if add(offer, json_file):
+            print(f"{label} offer added: {offer.get('مكان إيداع أو إرسال ملفات الترشح')}")
